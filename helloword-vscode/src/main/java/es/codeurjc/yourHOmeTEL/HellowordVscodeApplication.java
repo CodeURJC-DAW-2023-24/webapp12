c50d975adcd1c75ff8da263af0d2438d32b34bd1
Akram <pamonugi@gmail.com>
2024-02-20 15:03:59 +0100

    Security 1.0
    usuario en el codigo.
    
    La pantalla de login funciona un poco raro

--- helloword-vscode/src/main/java/es/codeurjc/yourHOmeTEL/HellowordVscodeApplication.java
+++ helloword-vscode/src/main/java/es/codeurjc/yourHOmeTEL/HellowordVscodeApplication.java
@@ -1,14 +1,14 @@
-package es.codeurjc.yourHOmeTEL.controller;
+package es.codeurjc.yourHOmeTEL;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication()
+@EnableAutoConfiguration()
 public class HellowordVscodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellowordVscodeApplication.class, args);
 	}
-
 }
