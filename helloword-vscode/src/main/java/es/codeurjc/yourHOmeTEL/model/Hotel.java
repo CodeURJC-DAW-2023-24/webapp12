a4d37e910af711c4199b1b94d6531d4d9d6f963c
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2024-03-03 18:19:36 +0100

    addHotel with photos implemented

--- helloword-vscode/src/main/java/es/codeurjc/yourHOmeTEL/model/Hotel.java
+++ helloword-vscode/src/main/java/es/codeurjc/yourHOmeTEL/model/Hotel.java
@@ -83,6 +83,15 @@ public class Hotel {
         this.reviews = reviews;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 
     public String getName() {
         return name;
