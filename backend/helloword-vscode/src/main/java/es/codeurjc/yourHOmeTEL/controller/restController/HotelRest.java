f06b6441d3a93645adc1b15eb41c2c53333eea7c
Carlos-Herman <104588315+Carlos-Herman@users.noreply.github.com>
2024-03-14 21:21:11 +0100

    Added edit controllers to HotelRest

--- backend/helloword-vscode/src/main/java/es/codeurjc/yourHOmeTEL/controller/restController/HotelRest.java
+++ backend/helloword-vscode/src/main/java/es/codeurjc/yourHOmeTEL/controller/restController/HotelRest.java
@@ -3,6 +3,7 @@ package es.codeurjc.yourHOmeTEL.controller.restController;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,20 +47,20 @@ import jakarta.servlet.http.HttpServletRequest;
 public class HotelRest {
 
 	interface HotelDetails
-            extends UserE.Basic, Hotel.Complete, Review.Basic, Room.Basic, Reservation.Basic {
-    }
+			extends UserE.Basic, Hotel.Complete, Review.Basic, Room.Basic, Reservation.Basic {
+	}
 
 	@Autowired
-    private ObjectMapper objectMapper;
+	private ObjectMapper objectMapper;
 
-    @PostConstruct
-    public void setup() {
-        objectMapper.setDefaultMergeable(true);
-    }
+	@PostConstruct
+	public void setup() {
+		objectMapper.setDefaultMergeable(true);
+	}
 
-    @Autowired
+	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	HotelService hotelService;
 
@@ -68,5 +70,108 @@ public class HotelRest {
 	@Autowired
 	RoomService roomService;
 
-    
+	/**
+	 * Goes to the edit page of a hotel so you can edit the values. It loads the
+	 * current hotel data in the form
+	 * 
+	 */
+	@GetMapping("/hotels/{id}")
+	public ResponseEntity<Hotel> hotelDataLoadingForEdition(
+			HttpServletRequest request,
+			Model model,
+			@PathVariable Long id) {
+
+		try {
+			UserE currentUser = userService.findByNick(request.getUserPrincipal().getName()).orElseThrow();
+			UserE foundUser = hotelService.findById(id).orElseThrow().getManager();
+
+			if (currentUser.equals(foundUser)) {
+				Hotel hotel = hotelService.findById(id).orElseThrow();
+				return ResponseEntity.ok(hotel);
+				
+			} else {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+			}
+
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		
+	}
+
+	/**
+	 * Replaces the old hotel data for the new one received from the form
+	 * 
+	 * @throws IOException
+	 */
+	@PutMapping("/hotels/{id}")
+	public ResponseEntity<Hotel> editHotel(Model model, HttpServletRequest request, Hotel newHotel, Integer room1,
+			Integer cost1,
+			Integer room2,
+			Integer cost2, Integer room3, Integer cost3, Integer room4, Integer cost4, @PathVariable Long id)
+			throws IOException {
+
+		Hotel hotel;
+
+		try {
+			UserE currentUser = userService.findByNick(request.getUserPrincipal().getName()).orElseThrow();
+			UserE foundUser = hotelService.findById(id).orElseThrow().getManager();
+
+			if (currentUser.equals(foundUser)) {
+				hotel = hotelService.findById(id).orElseThrow();
+
+				hotel.setName(newHotel.getName());
+				hotel.setLocation(newHotel.getLocation());
+				hotel.setDescription(newHotel.getDescription());
+
+
+			} else {
+				return ResponseEntity.badRequest().build();
+			}
+
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.notFound().build();
+		}
+
+		// PENDIENTE -> Refactorizar si se puede
+		{
+			if (room1 != null)
+				for (int i = 0; i < room1; i++) {
+					Room room = new Room(1, cost1, new ArrayList<>(), newHotel);
+					hotel.getRooms().add(room);
+					roomService.save(room);
+				}
+
+			if (room2 != null)
+				for (int i = 0; i < room2; i++) {
+					Room room = new Room(2, cost2, new ArrayList<>(), newHotel);
+					hotel.getRooms().add(room);
+					roomService.save(room);
+				}
+
+			if (room3 != null)
+				for (int i = 0; i < room3; i++) {
+					Room room = new Room(3, cost3, new ArrayList<>(), newHotel);
+					hotel.getRooms().add(room);
+					roomService.save(room);
+				}
+
+			if (room4 != null)
+				for (int i = 0; i < room4; i++) {
+					Room room = new Room(4, cost4, new ArrayList<>(), newHotel);
+					hotel.getRooms().add(room);
+					roomService.save(room);
+				}
+
+		}
+
+		hotelService.save(hotel);
+
+		model.addAttribute("hotel", hotel);
+
+		return ResponseEntity.ok().build();
+
+	}
+
 }
