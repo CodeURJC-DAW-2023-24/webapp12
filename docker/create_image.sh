0f916430fa90e97931845863ac2eacdd5d7d7401
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2024-03-16 21:55:42 +0100

    *docker compse implementation

---
+++ docker/create_image.sh
@@ -0,0 +1,8 @@
+#sets build context to parent and builds using Dockerfile from docker directory
+docker build --no-cache -t daarling/yourhometel -f Dockerfile ..
+
+#Pushes the image to dockerhub
+docker push daarling/yourhometel
+
+#Run the image using docker-compose from docker directory
+docker-compose up
