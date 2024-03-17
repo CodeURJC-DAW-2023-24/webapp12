bc823267aa2f779a0291415857b219fd868cb5b5
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2024-03-17 15:20:49 +0100

    docker changes

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,5 +1,5 @@
 #sets build context to parent and builds using Dockerfile from docker directory
-docker build -t daarling/yourhometel -f Dockerfile ..
+docker build -t daarling/yourhometel -f Dockerfile ../
 #Pushes the image to dockerhub
 docker push daarling/yourhometel
 #Run the image using docker-compose from docker directory
