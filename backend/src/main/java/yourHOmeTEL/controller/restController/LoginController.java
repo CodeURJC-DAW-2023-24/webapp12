c0baa091574a890ba2b10d189cd343a64d146df1
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2024-03-18 20:28:53 +0100

    updated login rest controller to work with form and json

--- backend/src/main/java/yourHOmeTEL/controller/restController/LoginController.java
+++ backend/src/main/java/yourHOmeTEL/controller/restController/LoginController.java
@@ -1,8 +1,10 @@
 package yourHOmeTEL.controller.restController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,18 @@ public class LoginController {
     @Autowired
     private UserLoginService userService;
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(
+    //form login
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<AuthResponse> loginForm(
+            @CookieValue(name = "accessToken", required = false) String accessToken,
+            @CookieValue(name = "refreshToken", required = false) String refreshToken,
+            @ModelAttribute LoginRequest loginRequest) {
+        return userService.login(loginRequest, accessToken, refreshToken);
+    }
+
+    //json login
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AuthResponse> loginJson(
             @CookieValue(name = "accessToken", required = false) String accessToken,
             @CookieValue(name = "refreshToken", required = false) String refreshToken,
             @RequestBody LoginRequest loginRequest) {
