67bc0735313b858225128293f805bb3fc4b826bf
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2024-04-07 01:11:27 +0200

    added global css and js imports to angular project

---
+++ frontend/src/app/profileScreens/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './footer.component.html',
+  //styleUrl: ''
+})
+export class AppComponent {
+  title = 'frontend';
+}
